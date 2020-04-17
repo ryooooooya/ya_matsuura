e624b009966bd958ad3f171a497a59d67163bea5
Chihara <chihara.ryoya@gmail.com>
2020-04-17 15:44:00 +0900

    upload

--- ya_matsuura/nuxt.config.js
+++ ya_matsuura/nuxt.config.js
@@ -1,4 +1,8 @@
+const envPath = `configs/.env.${process.env.ENV || 'development'}`
+require('dotenv').config({ path: envPath })
+
 export default {
+  srcDir: 'src/',
   mode: 'universal',
   /*
    ** Headers of the page
@@ -11,10 +15,15 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
-      }
+        content: process.env.npm_package_description || '',
+      },
+      {
+        hid: 'robots',
+        name: 'robots',
+        content: process.env.META_ROBOTS || 'index',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   /*
    ** Customize the progress-bar color
@@ -35,12 +44,27 @@ export default {
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
     // Doc: https://github.com/nuxt-community/stylelint-module
-    '@nuxtjs/stylelint-module'
+    '@nuxtjs/stylelint-module',
   ],
   /*
    ** Nuxt.js modules
    */
-  modules: [],
+  modules: [
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv',
+  ],
+
+  /*
+   ** Build configuration
+   */
+  env: {
+    CTF_SPACE_ID: process.env.CTF_SPACE_ID || '',
+    CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN || '',
+    CTF_PERSON_ID: process.env.CTF_PERSON_ID || '',
+    CTF_BLOG_POST_TYPE_ID: process.env.CTF_BLOG_POST_TYPE_ID || '',
+    CTF_PREVIEW_HOST: process.env.CTF_PREVIEW_HOST || '',
+  },
+
   /*
    ** Build configuration
    */
@@ -48,6 +72,6 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
-  }
+    extend(config, ctx) {},
+  },
 }
